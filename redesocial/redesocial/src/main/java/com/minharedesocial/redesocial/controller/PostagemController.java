4f6e2116c17b250277df5d7e9c335f3a2a1bee3b
Marcus Vinicius <62731107+MOliv3ira@users.noreply.github.com>
2020-08-05 09:27:26 -0300

    CRUD em API Rest com Spring Boot

--- redesocial/redesocial/src/main/java/com/minharedesocial/redesocial/controller/PostagemController.java
+++ redesocial/redesocial/src/main/java/com/minharedesocial/redesocial/controller/PostagemController.java
@@ -26,19 +26,19 @@ public class PostagemController {
 	@Autowired
 	public PostagemRepository repository;
 	
-	@GetMapping 	//Estou me refendo a um metodo Get para vir uma lista de Turma
+	@GetMapping 	//Estou me referindo a um metodo Get para vir uma lista de postagem
 	public ResponseEntity<List<Postagem>> GetAll(){
 		
 		return ResponseEntity.ok(repository.findAll()); 					//Chamando o Metodo FindAll para trazer(retornar) uma LISTA de Dados
 	}
 	
 	
-	@GetMapping("/{id}") 													//Defini que irei receber como um parametro atravez da URL //localhost:8080/turma/2  <-- o Numero 2 é o parametro
+	@GetMapping("/{id}") 													//Defini que irei receber como um parametro atravez da URL //localhost:8080/postagem/2  <-- o Numero 2 é o parametro
 	public ResponseEntity<Postagem> GetById(@PathVariable long id){  			// @PathVariable infoma que o valor virá da URL como paramentro
 		
 		return repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp)) 
-				.orElse(ResponseEntity.notFound().build()); 				// Metodo Map retorma uma valor do tipo turma e caso exista ele retorna um objeto do tipo 200 OK e caso não exista ele retorna um badResquest 404
+				.orElse(ResponseEntity.notFound().build()); 				// Metodo Map retorma uma valor do tipo postagem e caso exista ele retorna um objeto do tipo 200 OK e caso não exista ele retorna um badResquest 404
 	}
 	
 	 /*@GetMapping("/descricao/{descricao}")
@@ -48,7 +48,7 @@ public class PostagemController {
 	
 	
 	@PostMapping 															//Metodo Post, iremos postar (inserir) as informações em nosso banco de dados
-	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){  			//@RequestBody -> Informa que o objeto Turma virá via Body (Corpo da requisição)
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){  			//@RequestBody -> Informa que o objeto postagem virá via Body (Corpo da requisição)
 		
 		return ResponseEntity.status(HttpStatus.CREATED)  					//ResponseEntity.status(HttpStatus.CREATED) retorna o status 201 Create para o cliente
 				.body(repository.save(postagem));    							//.body(repository.save(turma)) retorna na body -- Corpo o objeto Criado
@@ -58,7 +58,7 @@ public class PostagemController {
 	
 	
 	@PutMapping																//Metodo que fará a Atualização nos dados que ja existem na nossa base
-	public ResponseEntity<Postagem> Put(@RequestBody Postagem postagem){				//@RequestBody -> Informa que o objeto Turma virá via Body (Corpo da requisição)
+	public ResponseEntity<Postagem> Put(@RequestBody Postagem postagem){				//@RequestBody -> Informa que o objeto postagem virá via Body (Corpo da requisição)
 		return ResponseEntity.status(HttpStatus.OK) 						//ResponseEntity.status(HttpStatus.OK) retorna o status 200 OK para o cliente
 				.body(repository.save(postagem)); 								//.body(repository.save(turma)) retorna na body -- Corpo o objeto Atualizado
 	}																		//Lembrando que quando o objeto vem com o ID estamos fazendo uma atualização.
--- redesocial/redesocial/src/main/java/com/minharedesocial/redesocial/controller/UsuarioController.java
+++ redesocial/redesocial/src/main/java/com/minharedesocial/redesocial/controller/UsuarioController.java
@@ -27,26 +27,26 @@ public class UsuarioController {
 	@Autowired
 	public UsuarioRepository repository;
 	
-	@GetMapping 	//Estou me refendo a um metodo Get para vir uma lista de Turma
+	@GetMapping 	//Estou me referinndo a um metodo Get para vir uma lista de usuario
 	public ResponseEntity<List<Usuario>> GetAll(){
 		
 		return ResponseEntity.ok(repository.findAll()); 					//Chamando o Metodo FindAll para trazer(retornar) uma LISTA de Dados
 	}
 	
 	
-	@GetMapping("/{id}") 													//Defini que irei receber como um parametro atravez da URL //localhost:8080/turma/2  <-- o Numero 2 é o parametro
+	@GetMapping("/{id}") 													//Defini que irei receber como um parametro atravez da URL //localhost:8080/usuario/2  <-- o Numero 2 é o parametro
 	public ResponseEntity<Usuario> GetById(@PathVariable long id){  			// @PathVariable infoma que o valor virá da URL como paramentro
 		
 		return repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp)) 
-				.orElse(ResponseEntity.notFound().build()); 				// Metodo Map retorma uma valor do tipo turma e caso exista ele retorna um objeto do tipo 200 OK e caso não exista ele retorna um badResquest 404
+				.orElse(ResponseEntity.notFound().build()); 				// Metodo Map retorma uma valor do tipo usuario e caso exista ele retorna um objeto do tipo 200 OK e caso não exista ele retorna um badResquest 404
 	}
 	
 	
 	
 	
 	@PostMapping 															//Metodo Post, iremos postar (inserir) as informações em nosso banco de dados
-	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){  			//@RequestBody -> Informa que o objeto Turma virá via Body (Corpo da requisição)
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){  			//@RequestBody -> Informa que o objeto usuario virá via Body (Corpo da requisição)
 		
 		return ResponseEntity.status(HttpStatus.CREATED)  					//ResponseEntity.status(HttpStatus.CREATED) retorna o status 201 Create para o cliente
 				.body(repository.save(usuario));    							//.body(repository.save(turma)) retorna na body -- Corpo o objeto Criado
@@ -56,7 +56,7 @@ public class UsuarioController {
 	
 	
 	@PutMapping																//Metodo que fará a Atualização nos dados que ja existem na nossa base
-	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario){				//@RequestBody -> Informa que o objeto Turma virá via Body (Corpo da requisição)
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario){				//@RequestBody -> Informa que o objeto usuario virá via Body (Corpo da requisição)
 		return ResponseEntity.status(HttpStatus.OK) 						//ResponseEntity.status(HttpStatus.OK) retorna o status 200 OK para o cliente
 				.body(repository.save(usuario)); 								//.body(repository.save(turma)) retorna na body -- Corpo o objeto Atualizado
 	}																		//Lembrando que quando o objeto vem com o ID estamos fazendo uma atualização.
